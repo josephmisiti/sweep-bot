301f967911cc9353f8f342bce8a1f82d9789bb0d
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-03-18 16:59:50 -0700

    Fix

--- src/utils/tiktoken_utils.py
+++ src/utils/tiktoken_utils.py
@@ -1,11 +1,11 @@
 import tiktoken
 
-encoding = tiktoken.encoding_for_model("gpt-4")
+
 def count_tokens(text: str) -> int:
-    tokenizer = Tokenizer()
+    encoding = tiktoken.encoding_for_model("gpt-4")
     try:
         tokens = encoding.encode(text)
         return len(tokens)
-    except TokenizerException as e:
+    except Exception as e:
         print(f"Error: {e}")
         return 0
