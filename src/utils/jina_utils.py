59384dac8a2497a733bf058d187a999fab3e7590
Peter Zhao <35006243+pzhao1799@users.noreply.github.com>
2023-03-20 22:39:58 -0700

    search

---
+++ src/utils/jina_utils.py
@@ -0,0 +1,17 @@
+import requests
+from pydantic import BaseModel
+
+
+class JinaClient(BaseModel):
+    url: str
+
+    def __init__(self, url: str):
+        super().__init__(url=url)
+
+    def search(self, query: str):
+        payload = {
+            "data": [{"text": query}],
+            "parameters": {}
+        }
+        response = requests.post(f'{self.url}/search', json=payload)
+        return response.json()
